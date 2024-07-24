fa4668b98bccbde61dffe3bf0408ffcc5521a66f
visal-krishnan <visalkrishnan01@gmail.com>
2024-07-24 17:48:59 +0530

    Changed to RestTemplate

---
+++ StateServices/src/main/java/com/ust/StateServices/StateServicesApplication.java
@@ -0,0 +1,25 @@
+package com.ust.StateServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class StateServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateServicesApplication.class, args);
+	}
+//	@Bean
+//	public WebClient webClient(){
+//		return WebClient.builder().build();
+//	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
